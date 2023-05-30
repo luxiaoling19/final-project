978ce80bc1749bb4003bc3e1eae8f3de51b4343b
luxiaoling19 <133432816+luxiaoling19@users.noreply.github.com>
2023-05-30 19:12:04 +0100

    Update and rename compare with known drivers.R to TP, TN calculation.R

--- TP, TN calculation.R
+++ TP, TN calculation.R
@@ -23,7 +23,8 @@ write_xlsx(histological_known_driver, excel_file)
 #determine whether driver is known
 result <- histological_driver_name$gene_name %in% COSMIC_known_drivers$gene_name
 print(result)
-table(result) #count number of TRUE and FALSE
+#count number of TRUE and FALSE
+table(result) 
 #############################################
 COSMIC_known_drivers_contain_lung <- COSMIC_known_drivers[grepl("gene_name", histological_driver_name$gene_name), ]
 
@@ -37,7 +38,6 @@ folder_path <- "C:/Users/92098/Desktop/driver genes data"
 excel_file <- file.path(folder_path, "major_SBS_known_drivers.xlsx")
 write_xlsx(major_SBS_known_driver, excel_file)
 
-#determine whether driver is known
 result <- major_SBS$gene_name %in% COSMIC_known_drivers$gene_name
 print(result)
 table(result)
@@ -51,7 +51,7 @@ SBS2_and_SBS13_known_drivers <- merge(SBS2_and_SBS13, COSMIC_known_drivers, by="
 
 excel_file <- file.path(folder_path, "SBS2_and_SBS13_known_drivers.xlsx")
 write_xlsx(SBS2_and_SBS13_known_drivers, excel_file)
-#determine whether driver is known
+
 result <- SBS2_and_SBS13$gene_name %in% COSMIC_known_drivers$gene_name
 print(result)
 table(result)
@@ -63,7 +63,7 @@ SBS2_or_SBS13_known_drivers <- merge(SBS2_or_SBS13, COSMIC_known_drivers, by="ge
 
 excel_file <- file.path(folder_path, "SBS2_or_SBS13_known_drivers.xlsx")
 write_xlsx(SBS2_or_SBS13_known_drivers, excel_file)
-#determine whether driver is known
+
 result <- SBS2_or_SBS13$gene_name %in% COSMIC_known_drivers$gene_name
 print(result)
 table(result)
@@ -76,28 +76,22 @@ SBS4_and_SBS92_known_drivers <- merge(SBS4_and_SBS92, COSMIC_known_drivers, by="
 
 excel_file <- file.path(folder_path, "SBS4_and_SBS92_known_drivers.xlsx")
 write_xlsx(SBS4_and_SBS92_known_drivers, excel_file)
-#determine whether driver is known
+
 result <- SBS4_and_SBS92$gene_name %in% COSMIC_known_drivers$gene_name
 print(result)
 table(result)
 #############################################
 SBS4_or_SBS92 <- read_excel("C:/Users/92098/Desktop/driver genes data/SBS4 & SBS 92.xlsx", 
-                             sheet = "或")
+                             sheet = "OR")
 SBS4_or_SBS92 <- na.omit(SBS4_or_SBS92[1:30,"gene_name"])
 SBS4_or_SBS92_known_drivers <- merge(SBS4_or_SBS92, COSMIC_known_drivers, by="gene_name")
 
 excel_file <- file.path(folder_path, "SBS4_or_SBS92_known_drivers.xlsx")
 write_xlsx(SBS4_or_SBS92_known_drivers, excel_file)
-#determine whether driver is known
+
 result <- SBS4_or_SBS92$gene_name %in% COSMIC_known_drivers$gene_name
 print(result)
 table(result)
-
-
-columns_to_search <- c("Name", "Tumour Types(Somatic)", "Cancer Syndrome","Other Syndrome") 
-
-COSMIC_known_drivers_contain_lung <- COSMIC_known_drivers[rowSums(sapply(COSMIC_known_drivers[columns_to_search], function(x) grepl("NSCLC", x))) > 0, ]
-nrow(COSMIC_known_drivers_contain_lung)
 
 
 
