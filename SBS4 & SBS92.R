456ce5ade1ed50f09a09500d50282ef1168cead3
luxiaoling19 <133432816+luxiaoling19@users.noreply.github.com>
2023-05-30 19:17:53 +0100

    Update SBS4 & SBS92.R

--- SBS4 & SBS92.R
+++ SBS4 & SBS92.R
@@ -17,10 +17,6 @@ colnames(mutation) = gsub('start', 'pos', colnames(mutation))
 ###### !!! to make chr readable for dndscvpackage(chr column only number)
 mutation$chr = gsub("chr","",as.vector(mutation$chr))
 head(mutation)
-# tumour_id  chr      pos ref var
-#1  CRUK0005 19 34291428   G   T
-#2  CRUK0005 19  9084770   C   A
-#3  CRUK0005  1  2160418   G   T
 rm(mut_table)
 ################################################################################
 
@@ -37,12 +33,10 @@ signature_weights = readRDS('figurecode/data/20221110_TRACERx421_mutationSignatu
 signature_weights_perTumour <- signature_weights[["signature_weights_perTumour"]]
 rm(signature_weights)
 
-
 signature_weights_perTumour <- signature_weights_perTumour[signature_weights_perTumour[, "SBS4"] > 0.1 & signature_weights_perTumour[, "SBS92"] > 0.1, 
                                                            "tumour_id", drop = FALSE] 
-#drop = FALSE:Output result in the form of a data frame (not a vector)
-head(signature_weights_perTumour)
 
+head(signature_weights_perTumour)
 ################################################################################
 SBS4_SBS92_both_0.1 <- merge(signature_weights_perTumour,mutation, by = "tumour_id")
 head(SBS4_SBS92_both_0.1)
@@ -73,6 +67,7 @@ signature_weights_perTumour <- signature_weights[["signature_weights_perTumour"]
 rm(signature_weights)
 
 SBS4_plus_SBS92 <- signature_weights_perTumour[signature_weights_perTumour[, "SBS4"] > 0.1 | signature_weights_perTumour[, "SBS92"] > 0.1,"tumour_id",drop = FALSE]
+
 head(SBS4_plus_SBS92)
 ###################################################################
 SBS4_plus_SBS92 <- merge(SBS4_plus_SBS92,mutation, by = "tumour_id")
