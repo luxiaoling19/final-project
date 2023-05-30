0894ed6e77c5636a33443d592334700c14caec99
luxiaoling19 <133432816+luxiaoling19@users.noreply.github.com>
2023-05-30 19:17:02 +0100

    Update SBS2 & SBS13.R

--- SBS2 & SBS13.R
+++ SBS2 & SBS13.R
@@ -17,10 +17,6 @@ colnames(mutation) = gsub('start', 'pos', colnames(mutation))
 ###### !!! to make chr readable for dndscvpackage(chr column only number)
 mutation$chr = gsub("chr","",as.vector(mutation$chr))
 head(mutation)
-# tumour_id  chr      pos ref var
-#1  CRUK0005 19 34291428   G   T
-#2  CRUK0005 19  9084770   C   A
-#3  CRUK0005  1  2160418   G   T
 rm(mut_table)
 ################################################################################
 
@@ -37,12 +33,10 @@ signature_weights = readRDS('figurecode/data/20221110_TRACERx421_mutationSignatu
 signature_weights_perTumour <- signature_weights[["signature_weights_perTumour"]]
 rm(signature_weights)
 
-
 signature_weights_perTumour <- signature_weights_perTumour[signature_weights_perTumour[, "SBS2"] > 0.1 & signature_weights_perTumour[, "SBS13"] > 0.1, 
                                                            "tumour_id", drop = FALSE] 
-#drop = FALSE:Output result in the form of a data frame (not a vector)
-head(signature_weights_perTumour)
 
+head(signature_weights_perTumour)
 ################################################################################
 SBS2_SBS13_both_0.1 <- merge(signature_weights_perTumour,mutation, by = "tumour_id")
 head(SBS2_SBS13_both_0.1)
@@ -74,6 +68,7 @@ rm(signature_weights)
 
 SBS2_plus_SBS13 <- signature_weights_perTumour[signature_weights_perTumour[, "SBS2"] > 0.1 | signature_weights_perTumour[, "SBS13"] > 0.1, 
                                                            "tumour_id", drop = FALSE]
+
 head(SBS2_plus_SBS13)
 ###################################################################
 SBS2_plus_SBS13 <- merge(SBS2_plus_SBS13,mutation, by = "tumour_id")
@@ -90,8 +85,6 @@ print (dndsout_SBS2_plus_SBS13$globaldnds)
 print(dndsout_SBS2_plus_SBS13$nbreg$theta)
 
 AIC(dndsout_SBS2_plus_SBS13$poissmodel)
-
-
 
 ###################################################################
 major_SBS2_OR_SBS13 <- max_SBS[grepl("SBS2|SBS13",max_SBS$max_sig_weights),"tumour_id"]
